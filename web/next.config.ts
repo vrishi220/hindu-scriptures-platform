016eaa877ef5d988cb936c7fba98e93071c51442
vrishi220 <vrishi@gmail.com>
2026-02-16 16:01:12 -0500

    Update next.config.ts

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+	async rewrites() {
+		return [
+			{
+				source: "/api/users/me",
+				destination: "/api/me",
+			},
+		];
+	},
+};
 
 export default nextConfig;
